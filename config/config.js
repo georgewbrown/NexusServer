8642d9e3a2cea96ff5f62bfcbcd41f165fc433c0
George <george@Georges-MacBook-Pro.local>
2018-12-07 11:28:52 -0500

    fixed seeding

--- config/config.js
+++ config/config.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
 module.exports = {
-  development: {
-    username: process.env.PGUSERNAME,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    host: '127.0.0.9',
-    dialect: 'postgres'
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'postgres'
-  },
+  // development: {
+  //   username: process.env.PGUSERNAME,
+  //   password: process.env.PGPASSWORD,
+  //   database: process.env.PGDATABASE,
+  //   host: '127.0.0.1',
+  //   dialect: 'postgres'
+  // },
+  // test: {
+  //   username: 'root',
+  //   password: null,
+  //   database: 'database_test',
+  //   host: '127.0.0.1',
+  //   dialect: 'postgres'
+  // },
   production: {
     username: process.env.NAME,
     password: process.env.PASS,
